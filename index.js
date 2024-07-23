d9f5ed3b0fd2bc5adbf8ae457d1a9ac13887d26e
Gamal Ahmed <gamalahmed3265@gmail.com>
2024-07-23 19:04:27 +0300

    CURD Operation added

--- index.js
+++ index.js
@@ -11,8 +11,94 @@ const PORT=process.env.PORT || 6000
 
 app.use(express.json())
 
-app.get("/",(req,res)=>{
-    res.send("Good")
+app.post("/",async(req,res)=>{
+    
+    console.log(req.body);
+    const {
+        firstName,
+        lastName,
+        age
+    }=req.body;
+
+    const newUser=await prisma.user.create({
+        data:{
+            firstName,
+            lastName,
+            age:+age
+        }
+    })
+    res.json({
+        message:"User is Created",
+        user:newUser
+    });
+})
+
+app.get("/",async(req,res)=>{
+    try {
+        const allUser=await prisma.user.findMany()
+        
+        res.json({
+            message:"Users is returned",
+            status:200,
+            users:allUser
+        });
+        
+    } catch (error) {
+        res.json({
+            message:error,
+            status:400
+        });
+    }
+})
+
+app.patch("/:id",async(req,res)=>{
+    try {
+        const id=req.params.id;
+        const {age}=req.body;
+        
+        const updateUser=await prisma.user.update({
+            where:{
+                id:parseInt(id)
+            },
+            data:{
+                age
+            }
+        })
+        
+        res.json({
+            message:"Users is Updated",
+            status:200,
+            user:updateUser
+        });
+
+    } catch (error) {
+        res.json({
+            message:error,
+            status:400
+        });
+
+    }
+})
+app.delete("/:id",async(req,res)=>{
+    try {
+        const id=req.params.id;        
+        await prisma.user.delete({
+            where:{
+                id:parseInt(id)
+            },
+        })
+        res.json({
+            message:"Users is Deleted",
+            status:200,
+        });
+
+    } catch (error) {
+        res.json({
+            message:error,
+            status:400
+        });
+
+    }
 })
 
 app.listen(PORT,()=>{
